146fd8b76d5cecd12e998c5133355ad83848bb0e
JesusGarciaSantiago <jesusgs0729@gmail.com>
2024-07-14 13:43:47 -0600

    Get with name, add README and another dependencies

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ app.get ("/", (req, res, next)=>{
     res.send ("Bienvenido al servidor :c");
 
 });
-app.listen(3000, () =>{
+
+app.get ("/:name", (req, res, next) =>{
+    console.log(req.params.name);
+    res.status (200);
+    res.send("Hola, " + req.params.name);
+});
+
+app.listen(process.env.PORT || 3000, () =>{
     console.log ("Server is running...");
 });
