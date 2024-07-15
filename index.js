ffe3488648a4fb00f3e630c4850adf94c5801cfd
JesusGarciaSantiago <jesusgs0729@gmail.com>
2024-07-15 16:09:14 -0600

    Ternary operator and filter method

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ const { pokemon } = require ('./pokedex.json');
 
 /*
 *Verbos HTTP
-*GET
-*POST
-*PATCH
-*PUT
-*DELETE
+*GET - Obtener recursos
+*POST - Almacenar/crear recursos
+*PATCH - Modificar una parte de un recurso
+*PUT - Modificar todo el recurso
+*DELETE - Borrar un recurso
 */
 
 app.get ("/", (req, res, next)=>{
-    res.status(200);
-    res.send ("Bienvenido al Pokedex");
+    
+   return res.status(200).send ("Bienvenido al Pokedex");
 
 });
 
-app.get ("/pokemon/all", (req, res, next) =>{
-    res.status (200);
-    res.send(pokemon)
+app.get ("/pokemon", (req, res, next) =>{
+   
+    return res.status(200).send(pokemon)
     
 });
 
@@ -27,29 +27,31 @@ app.get ("/pokemon/all", (req, res, next) =>{
 
 app.get ('/pokemon/:id([0-9]{1,3})', (req, res, next) => {
     const id = req.params.id -1;
-    if (id >= 0 && id <= 150){
-        res.status(200);
-        res.send(pokemon[req.params.id - 1]); 
-    }else{
-        res.status(404);
-        res.send ("Pokémon no encontrado");
+    if (id >= 0 && id <= 150){        
+        return res.status(200).send(pokemon[req.params.id - 1]); 
     }
+        return res.status(404).send ("Pokémon no encontrado");
+
     
      
 }); 
 
-app.get ('/pokemon/:name', (req, res, next) => {
+app.get ('/pokemon/:name([A-Za-z]+)', (req, res, next) => {
+    
+    // Operador Ternario
+    // Condicion a evaluar ? valor si verdadero : valor si falso
+    
+    
     const name = req.params.name;
-    for (i = 0; i < pokemon.length; i++){
-        if(pokemon[i].name == name){
-            res.status(200);
-            res.send(pokemon[i])
-        }
-    }
-    res.status (404);
-    res.send ("Pokémon no encontrado");
+    const pk = pokemon.filter((p) => {
+       return (p.name.toUpperCase() == name.toUpperCase()) ? p : null;    
+    });
+    
+    (pk.length > 0) ? res.status(200).send(pk) :  res.status(404).send("Pokémon no encontrado") ;
+    
+    
 } );
 
 app.listen(process.env.PORT || 3000, () =>{
-    console.log ("Server is running...");
+    return console.log ("Server is running...");
 });
