111c04a0df325df12b52fd2a117b8ee6991de160
JesusGarciaSantiago <jesusgs0729@gmail.com>
2024-07-18 16:41:54 -0600

    added DELETE, PUT and PATCH

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -2,6 +2,7 @@ const express = require ('express');
 const pokemon = express.Router();
 const db = require ('../config/database');
 
+
 pokemon.post("/", async (req, res ,next) => {
     const {pok_name, pok_height, pok_weight, pok_base_experience} = req.body;
 
@@ -20,6 +21,51 @@ pokemon.post("/", async (req, res ,next) => {
     return res.status(500).json({code:500, massage:"Campos incompletos"});
 
 });
+
+pokemon.delete("/:id([0-9]{1,3})", async (req, res, next) =>{
+    const query =  `DELETE FROM pokemon WHERE pok_id  = ${req.params.id}` ;
+    const rows = await db.query(query);
+
+    if (rows.affectedRows == 1){
+        return res.status(200).json ({code:200, massage: "Pokemon borrado correctamente"});
+
+    }
+    return res.status(404).json({code: 404,message: "Pokemon no encontrado"});
+
+});
+
+
+
+
+pokemon.put("/:id([0-9]{1,3})", async (req, res, next) =>{
+    const {pok_name, pok_height, pok_weight, pok_base_experience} = req.body;
+    if (pok_name && pok_height && pok_weight && pok_base_experience){
+        let query =  `UPDATE pokemon SET pok_name='${pok_name}', pok_height =${pok_height},`;
+        query += `pok_weight =${pok_weight}, pok_base_experience=${pok_base_experience} WHERE pok_id = ${req.params.id}`;
+        const rows =  await db.query (query);
+        
+        if(rows.affectedRows == 1){
+            return res.status(200).json({code:200, massage: "Pokémon actualizado correctamente"});
+
+        }
+        return res.status(500).json({code: 500, massage: "Ocurrio un error"});
+    }
+    return res.status(500).json({code:500, massage:"Campos incompletos"});
+});
+
+pokemon.patch("/:id([0-9]{1,3})", async (req, res, next) =>{
+    if(req.body.pok_name){
+        let query = `UPDATE pokemon SET pok_name= '${req.body.pok_name}' WHERE pok_id = ${req.params.id}`;
+        const rows = await db.query(query);
+        if(rows.affectedRows == 1){
+            return res.status(200).json({code:200, massage: "Pokemon actualizado correctamente"});
+
+        }
+        return res.status(500).json ({code:500, message: "Ocurrio un error"});
+    }
+    return res.status(500).json ({code:500, message: "Campos incompletos"});
+});
+
 
 pokemon.get ("/", async (req, res, next) =>{
 
